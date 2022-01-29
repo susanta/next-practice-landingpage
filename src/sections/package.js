ef58b23326cede85f39f329f97c1589e40c7d6f3
Susanta <inbox.susanta@gmail.com>
2022-01-29 20:44:38 +0530

    Added package section plan buttons

--- src/sections/package.js
+++ src/sections/package.js
@@ -241,6 +241,24 @@ const responsive = {
 
 export default function Package() {
   const { monthly, annual } = packages;
+  const [planState, setPlanState] = useState({
+    active: 'monthly',
+    pricingPlan: monthly,
+  });
+
+  const handlePricingPlan = (plan) => {
+    if (plan === 'annual') {
+      setPlanState({
+        active: 'annual',
+        pricingPlan: annual,
+      });
+    } else {
+      setPlanState({
+        active: 'monthly',
+        pricingPlan: monthly,
+      });
+    }
+  };
 
   const sliderParams = {
     additionalTransfrom: 0,
@@ -266,7 +284,31 @@ export default function Package() {
   };
 
   return (
-    <h1>Package</h1>
+    <section id="pricing" sx={{ variant: 'section.pricing' }}>
+      <Container>
+        <SectionHeader slogan="Pricing Plan" title="Choose your pricing plan" />
+        <Flex sx={styles.buttonGroup}>
+          <Box sx={styles.buttonGroupInner}>
+            <button
+              className={planState.active === 'monthly' ? 'active' : ''}
+              type="button"
+              aria-label="Monthly"
+              onClick={() => handlePricingPlan('monthly')}
+            >
+              Monthly Plan
+            </button>
+            <button
+              className={planState.active === 'annual' ? 'active' : ''}
+              type="button"
+              aria-label="Annual"
+              onClick={() => handlePricingPlan('annual')}
+            >
+              Annual Plan
+            </button>
+          </Box>
+        </Flex>
+      </Container>
+    </section>
   );
 }
 
