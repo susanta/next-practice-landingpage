3ac9c186338803c22fc3fb90b1777449688410d5
Susanta <inbox.susanta@gmail.com>
2022-01-29 18:43:37 +0530

    Added feature section

--- src/sections/feature.js
+++ src/sections/feature.js
@@ -45,7 +45,26 @@ const data = [
 
 export default function Feature() {
   return (
-   <h1>Feature</h1>
+    <section sx={{ variant: 'section.feature' }}>
+      <Container>
+        <SectionHeader
+          slogan="Quality Features"
+          title="Amazing useful features"
+        />
+
+        <Grid sx={styles.grid}>
+          {data.map((dataitem) => (
+            <FeatureCard
+              key={dataitem.id}
+              src={dataitem.imgSrc}
+              alt={dataitem.altText}
+              title={dataitem.title}
+              text={dataitem.text}
+            />
+          ))}
+        </Grid>
+      </Container>
+    </section>
   );
 }
 
