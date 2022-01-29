201411ce9ac879920a182de0a6a019e12520e3f2
Susanta <inbox.susanta@gmail.com>
2022-01-29 17:36:45 +0530

    Added keyfeature section

--- src/components/section-header.js
+++ src/components/section-header.js
@@ -3,6 +3,26 @@ import { Box, Text, Heading } from 'theme-ui';
 
 export default function SectionHeader({ title, slogan, isWhite }) {
   return (
-    <h1>SectionHeader</h1>
+    <Box sx={{ variant: 'sectionHeader' }}>
+      <Text
+        as="P"
+        sx={{
+          variant: 'sectionHeader.subTitle',
+          color: isWhite ? 'white' : 'primary',
+          opacity: isWhite ? 0.7 : 1,
+        }}
+      >
+        {slogan}
+        <Heading
+          as="h2"
+          sx={{
+            variant: 'sectionHeader.title',
+            color: isWhite ? 'white' : 'heading',
+          }}
+        >
+          {title}
+        </Heading>
+      </Text>
+    </Box>
   );
 }
